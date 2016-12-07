e2ac6959f545f4c58b9be6b88ed58cce5d856082
Mariusz <mariusz.bancer@gmail.com>
2016-12-07 22:03:11 +0100

    program 0-2 correct, error 1-2 correct

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 class Analyzer:
 
     def __init__(self, parse_tree):
@@ -7,24 +9,28 @@ class Analyzer:
         self.global_variables = []
         self.used_variables = []
         self.declared_variables = [declaration[1] for declaration in self.declarations]
-        self.initialised_variables = []
+        self.alread_inicialised = []
 
     def analyze(self):
-        self.undeclared_variables()
-        self.redeclaration()
+        self.undeclared_variables(self.commands)
+        self.redeclaration([])
         self.initialised_variables(self.commands)
 
-    def undeclared_variables(self):
-        for command in self.commands:
-            if command[0] == "read" or command[0] == "write":
+    def undeclared_variables(self, commands):
+        for command in commands:
+            # if command[0] == "read" or command[0] == "write" or command[0] == "assign":
+            if  isinstance(command, tuple) and len(command) == 3:
                 self.check_read_write(command)
-            # self.check_command(command)
+            if isinstance(command, tuple) and len(command) != 3:
+                if not command[0].__str__().startswith("for"):
+                    self.undeclared_variables(command)
+
+                    # self.check_command(command)
             # if command[0] == "int" and command[1] not in already_declared:
             #     print "Variable", command[1], "in line", command[2], "not declared"
         # print(already_declared)
 
-    def redeclaration(self):
-        already_declared = []
+    def redeclaration(self, already_declared):
         for declaration in self.declarations:
             if declaration[1] in already_declared:
                 print "Redeclaration of:", declaration[1], "in line", declaration[2]
@@ -32,8 +38,8 @@ class Analyzer:
 
     def initialised_variables(self, commands):
         for command in commands:
-            if command[0] == "assign" or command[0] == "read":
-                self.initialised_variables.append(command[1][1])
+            if isinstance(command, tuple) and (command[0] == "assign" or command[0] == "read"):
+                self.alread_inicialised.append(command[1][1])
                 print command
             if isinstance(command, tuple) and len(command) != 3:
                 self.initialised_variables(command)
@@ -45,5 +51,5 @@ class Analyzer:
             print command[3]
 
     def check_read_write(self, command):
-        if command[1][1] not in self.already_declared:
-            print "Variable", command[1][1], "in line", command[1][2], "not declared"
+        if isinstance(command[2], int) and command[1] not in self.declared_variables:
+            print "Variable", command[1], "in line", command[2], "not declared"
